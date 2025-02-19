35c03c36ce48227623f39ebe9ea45dc46dc62580
NicholasAstor <nicholas.barbosa@edu.pucrs.br>
2025-02-19 12:52:30 -0300

    Feat: Login view and UpdatePassword view made

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Signup
+from .views import Signup, Login, UpdatePassword
 
 urlpatterns = [
-    path('signup/', Signup.as_view()),
+    path('signup/', Signup.as_view(), name='Signup'),
+    path('login/', Login.as_view(), name='Login'),
+    path('update_password/', UpdatePassword.as_view(), name='UpdatePassword'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
 
 from .models import DbUser
 
@@ -23,7 +24,7 @@ class Signup(APIView):
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
-        name = request.data.get('name')
+        username = request.data.get('username')
 
         if not email or not password:
             return Response({"status": "400", "success":False, "error": True, "message": "Email e senha são necessários", "data":None}, status=status.HTTP_400_BAD_REQUEST)
@@ -34,7 +35,7 @@ class Signup(APIView):
             return Response({"status": "400", "success":False, "error": True, "message": "Usuário já está registrado no sistema", "data":None}, status=status.HTTP_400_BAD_REQUEST)
         
         
-        new_user = User.objects.create(username=name, password=password)
+        new_user = User.objects.create(username=username, password=password, email=email)
         
 
         if not new_user:
@@ -45,8 +46,51 @@ class Signup(APIView):
 
         tokens = get_tokens_for_user(user=new_user)
 
-        novo_usuario = DbUser.objects.create(name=name, email=email)
+        novo_usuario = DbUser.objects.create(name=username, email=email)
 
         return Response({"status": "201", "success":True, "error": False, "message": "Sucesso", "data":{"user_id": novo_usuario.id, "tokens": tokens}}, status=status.HTTP_201_CREATED)
 
+class Login(APIView):
+    permission_classes = [AllowAny]
 
+    def post(self, request):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password or not username:
+            return Response({"status": "400", "success":False, "error": True, "message": "Email, senha e Nome de Usuário são necessários", "data":None}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return Response({"status": "400", "success":False, "error": True, "message": "Email, senha ou Nome de Usuário incorreto", "data":None}, status=status.HTTP_400_BAD_REQUEST)
+
+        if check_password(password, user.password) is False:
+            return Response({"status": "400", "success":False, "error": True, "message": "Email, senha ou Nome de Usuário incorreto", "data":None}, status=status.HTTP_400_BAD_REQUEST)
+
+        tokens = get_tokens_for_user(user=user)
+
+        dbuser = DbUser.objects.filter(email=email).first()
+
+        return Response({"status": "200", "success":True, "error": False, "message": "Sucesso", "data":{"user_id": dbuser.id, "tokens": tokens}}, status=status.HTTP_200_OK)
+    
+class UpdatePassword(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+
+        if not old_password or not new_password:
+            return Response({"status": "400", "success":False, "error": True, "message": "Senha atual e futura são necessárias para atualizar", "data":None}, status=status.HTTP_400_BAD_REQUEST)
+            
+        if check_password(old_password, request.user.password) is False:
+            return Response({"status": "400", "success":False, "error": True, "message": "Senha incorreta", "data":None}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = request.user
+        user.set_password(new_password)
+        user.save()
+
+        return Response({"status": "201", "success":True, "error": False, "message": "Senha atualizada com sucesso", "data":{"user_password": user.password}}, status=status.HTTP_201_CREATED)
